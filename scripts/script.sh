ee96bf26ac72d262b2cdf40ce863b09fde526ede
Tim Irnich <tim.irnich@suse.com>
2022-08-12 11:25:07 +0200

    Add reminder to deploy Provo specific hacks

--- scripts/script.sh
+++ scripts/script.sh
@@ -1378,6 +1378,7 @@ Default()
     EnsureExternalDNSIsDeployed
     EnsureSnippetAnnotationsAreAllowed # we need to patch Ingresses to allow connection upgrades
     EnsureLargeClientHeaderBuffersAreAllowed # this is needed for elemental-register's smbios transfer via HTTP header
+    #DeployHacks # this is specific to the Provo lab, remove or modify for other labs
     EnsureMediaIsDeployed
     EnsureIronicIsDeployed
     EnsureBareMetalOperatorIsDeployed
